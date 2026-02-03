1b98cfe5482a425bf09c782887be216c3d5aa88e
LuizBlume <luiz.fernando.251107@gmail.com>
2026-02-02 22:51:17 -0300

    Aula 3: Entendendo o JSX.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Luiz';
+  const newName = name.toUpperCase();
+  function sum(a, b) {
+    return a + b;
+  }
+  const url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA-7UIYLcEK1mAwct1XgW8aSMnvm3ZEQBYgQ&s";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá {newName}!</h1>
+      <h2>Soma dos valores é {sum(1, 2)}</h2>
+      <img src={url} alt='Minha imagem teste' />
     </div>
   );
 }
