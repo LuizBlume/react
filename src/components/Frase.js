2f09c7364303fc82e0be97e4fb939e5805ba708a
LuizBlume <luiz.fernando.251107@gmail.com>
2026-04-10 17:59:51 -0300

    Fix indentation in Frase component

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -2,7 +2,7 @@ import styles from './Frase.module.css';
 import HelloWorld from "./HelloWorld"
 function Frase() {
     return (
-            <div className={styles.fraseContainer}>
+        <div className={styles.fraseContainer}>
             <h1 className={styles.fraseContent}>Esse é o componente do <HelloWorld /></h1>
         </div>
     )
