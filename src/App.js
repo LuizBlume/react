2bf6520d0ec814899b034639d0e81e796aad8ae5
LuizBlume <luiz.fernando.251107@gmail.com>
2026-04-13 10:43:26 -0300

    FEAT: Create routes and pages for project.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
 import './App.css';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
 
 function App() {
-  const [nome, setNome] = useState();
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome} />
-      <Saudacao nome={nome} />
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/empresa" element={<Empresa />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
