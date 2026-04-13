06c969ee55792c8afa5724e11615085cf8768a52
LuizBlume <luiz.fernando.251107@gmail.com>
2026-04-13 09:04:21 -0300

    FEAT: Create files for a lesson on conditionals.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import './App.css';
-import Frase from './components/Frase';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import List from './components/List'
-import Evento from './components/Evento';
-import Form from './components/Form';
-
+import Condicional from './components/Condicional.js';
 function App() {
-  const name = 'Luiz';
-  const newName = name.toUpperCase();
-  function sum(a, b) {
-    return a + b;
-  }
-  const url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA-7UIYLcEK1mAwct1XgW8aSMnvm3ZEQBYgQ&s";
   return (
     <div className="App">
-      <h1>Olá {newName}!</h1>
-      <h2>Soma dos valores é {sum(1, 2)}</h2>
-      <img src={url} alt='Minha imagem teste' />
-      <Frase />
-      <SayMyName name="Ganso" />
-      <SayMyName name={name} />
-      <Pessoa name={name} age="18" profession="Programador" photo={url} />
-      <List />
-      <Evento numero="1" />
-      <Evento numero="2" />
-      <Form />
+      <h1>Renderização condicional</h1>
+      <Condicional />
     </div>
   );
 }
