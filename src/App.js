5d3049d9f491857d57a6ffbd53ecef076123db26
LuizBlume <luiz.fernando.251107@gmail.com>
2026-04-13 09:30:07 -0300

    FEAT: Create files and logic for list rendering.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Condicional from './components/Condicional.js';
+import OutraLista from './components/OutraLista';
 function App() {
+  const meusItens = ['React', 'Vue', 'Angular', 'PHP'];
   return (
     <div className="App">
-      <h1>Renderização condicional</h1>
-      <Condicional />
+      <h1>Renderização de listas</h1>
+      <OutraLista itens={meusItens} />
+      <OutraLista itens={[]} />
     </div>
   );
 }
